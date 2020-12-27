74b44c89ab2544918abdafa9ef7c38def5068778
kaen <kusdsuna@naver.com>
2020-12-28 00:18:32 +0900

    Add 숫자야구 folder

---
+++ 숫자야구/app.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,4,5,6,7,8,9];
+
+let numberArray = [];
+
+for (let i = 0; i < 4; i += 1) {
+    let result = numbers.splice(Math.floor(Math.random() * (9 -i)), 1)[0];
+    numberArray.push(result);
+}
+
+console.log(numberArray);
+
+// 배열.join('') -> 문자
+// 문자.split('') -> 배열
